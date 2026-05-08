4fe5b3d2b56a0ef8e5bba3aa73d448988a91b55c
z1n3p <denny.blomryd@me.com>
2026-05-08 20:04:59 +0200

    Fixar bas-sökväg

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: './',
+  plugins: [react()],
 })
